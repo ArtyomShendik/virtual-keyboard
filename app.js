2b8a4a96727e600e4239684413c4190cb7ba4641
Artyom Shendik <samok99@bk.ru>
2023-04-29 18:19:39 +0300

    fix: update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-console.log('aa')
 
 const body = document.body;
-
 const container = document.createElement('div');
 container.classList.add('container');
 body.prepend(container);
@@ -20,72 +18,48 @@ for (let i = 0; i < 5; i++) {
   keyboardKeys.appendChild(row)
 }
 
-const tilda = document.createElement('button');
-tilda.classList.add('keys');
-tilda.innerHTML = '`';
+document.onkeypress = function(e) {
+  console.log(fourthRow.push(e.charCode))
+}
 
 const row1 = document.querySelectorAll('.row')[0];
-row1.appendChild(tilda);
+const row2 = document.querySelectorAll('.row')[1];
+const row3 = document.querySelectorAll('.row')[2];
+const row4 = document.querySelectorAll('.row')[3];
 
-const one = document.createElement('button');
-one.classList.add('keys');
-one.innerHTML = '1';
-row1.appendChild(one);
+const firstRow = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61];
+const secondRow = [113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92];
+const thirdRow = [97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39];
+const fourthRow = [122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
 
-const two = document.createElement('button');
-two.classList.add('keys');
-two.innerHTML = '2';
-row1.appendChild(two);
+const init = () => {
+  let out = '';
+  let out2 = '';
+  let out3 = '';
+  let out4 = '';
 
-const three = document.createElement('button');
-three.classList.add('keys');
-three.innerHTML = '3';
-row1.appendChild(three);
+  for(let i = 0; i < firstRow.length; i++) {
+    out += `<button class="keys" data="">`+String.fromCharCode(firstRow[i])+`</button>`
+  }
+  row1.innerHTML = out;
 
-const four = document.createElement('button');
-four.classList.add('keys');
-four.innerHTML = '4';
-row1.appendChild(four);
+  for(let i = 0; i < secondRow.length; i++) {
+    out2 += `<button class="keys" data="">`+String.fromCharCode(secondRow[i])+`</button>`
+  }
+  row2.innerHTML = out2;
 
-const five = document.createElement('button');
-five.classList.add('keys');
-five.innerHTML = '5';
-row1.appendChild(five);
+  for(let i = 0; i < thirdRow.length; i++) {
+    out3 += `<button class="keys" data="">`+String.fromCharCode(thirdRow[i])+`</button>`
+  }
+  row3.innerHTML = out3;
 
-const six = document.createElement('button');
-six.classList.add('keys');
-six.innerHTML = '6';
-row1.appendChild(six);
+  for(let i = 0; i < fourthRow.length; i++) {
+    out4 += `<button class="keys" data="">`+String.fromCharCode(fourthRow[i])+`</button>`
+  }
+  row4.innerHTML = out4;
+}
 
-const seven = document.createElement('button');
-seven.classList.add('keys');
-seven.innerHTML = '7';
-row1.appendChild(seven);
-
-const eight = document.createElement('button');
-eight.classList.add('keys');
-eight.innerHTML = '8';
-row1.appendChild(eight);
-
-const nine = document.createElement('button');
-nine.classList.add('keys');
-nine.innerHTML = '9';
-row1.appendChild(nine);
-
-const zero = document.createElement('button');
-zero.classList.add('keys');
-zero.innerHTML = '0';
-row1.appendChild(zero);
-
-const dash = document.createElement('button');
-dash.classList.add('keys');
-dash.innerHTML = '-';
-row1.appendChild(dash);
-
-const equals = document.createElement('button');
-equals.classList.add('keys');
-equals.innerHTML = '=';
-row1.appendChild(equals);
+init();
 
 const backspace = document.createElement('button');
 backspace.classList.add('keys');
@@ -93,148 +67,17 @@ backspace.classList.add('backspace-key');
 backspace.innerHTML = 'Backspace';
 row1.appendChild(backspace);
 
-
-// second row
-
-const row2 = document.querySelectorAll('.row')[1];
-
 const tab = document.createElement('button');
 tab.classList.add('keys');
 tab.classList.add('tab-key');
 tab.innerHTML = 'Tab';
-row2.appendChild(tab);
-
-const q = document.createElement('button');
-q.classList.add('keys');
-q.innerHTML = 'q';
-row2.appendChild(q);
-
-const w = document.createElement('button');
-w.classList.add('keys');
-w.innerHTML = 'w';
-row2.appendChild(w);
-
-const e = document.createElement('button');
-e.classList.add('keys');
-e.innerHTML = 'e';
-row2.appendChild(e);
-
-const r = document.createElement('button');
-r.classList.add('keys');
-r.innerHTML = 'r';
-row2.appendChild(r);
-
-const t = document.createElement('button');
-t.classList.add('keys');
-t.innerHTML = 't';
-row2.appendChild(t);
-
-const y = document.createElement('button');
-y.classList.add('keys');
-y.innerHTML = 'y';
-row2.appendChild(y);
-
-const u = document.createElement('button');
-u.classList.add('keys');
-u.innerHTML = 'u';
-row2.appendChild(u);
-
-const i = document.createElement('button');
-i.classList.add('keys');
-i.innerHTML = 'i';
-row2.appendChild(i);
-
-const o = document.createElement('button');
-o.classList.add('keys');
-o.innerHTML = 'o';
-row2.appendChild(o);
-
-const p = document.createElement('button');
-p.classList.add('keys');
-p.innerHTML = 'p';
-row2.appendChild(p);
-
-const braceRight = document.createElement('button');
-braceRight.classList.add('keys');
-braceRight.innerHTML = '{';
-row2.appendChild(braceRight);
-
-const braceLeft = document.createElement('button');
-braceLeft.classList.add('keys');
-braceLeft.innerHTML = '}';
-row2.appendChild(braceLeft);
-
-const slash = document.createElement('button');
-slash.classList.add('keys');
-slash.classList.add('slash-key');
-slash.innerHTML = "/";
-row2.appendChild(slash);
-
-
-// third row
-
-const row3 = document.querySelectorAll('.row')[2];
+row2.prepend(tab);
 
 const capslock = document.createElement('button');
 capslock.classList.add('keys');
 capslock.classList.add('capslock-key');
 capslock.innerHTML = 'Caps Lock';
-row3.appendChild(capslock);
-
-const a = document.createElement('button');
-a.classList.add('keys');
-a.innerHTML = 'a';
-row3.appendChild(a);
-
-const s = document.createElement('button');
-s.classList.add('keys');
-s.innerHTML = 's';
-row3.appendChild(s);
-
-const d = document.createElement('button');
-d.classList.add('keys');
-d.innerHTML = 'd';
-row3.appendChild(d);
-
-const f = document.createElement('button');
-f.classList.add('keys');
-f.innerHTML = 'f';
-row3.appendChild(f);
-
-const g = document.createElement('button');
-g.classList.add('keys');
-g.innerHTML = 'g';
-row3.appendChild(g);
-
-const h = document.createElement('button');
-h.classList.add('keys');
-h.innerHTML = 'h';
-row3.appendChild(h);
-
-const j = document.createElement('button');
-j.classList.add('keys');
-j.innerHTML = 'j';
-row3.appendChild(j);
-
-const k = document.createElement('button');
-k.classList.add('keys');
-k.innerHTML = 'k';
-row3.appendChild(k);
-
-const l = document.createElement('button');
-l.classList.add('keys');
-l.innerHTML = 'l';
-row3.appendChild(l);
-
-const semicolon = document.createElement('button');
-semicolon.classList.add('keys');
-semicolon.innerHTML = ';';
-row3.appendChild(semicolon);
-
-const quotes = document.createElement('button');
-quotes.classList.add('keys');
-quotes.innerHTML = "'";
-row3.appendChild(quotes);
+row3.prepend(capslock);
 
 const enter = document.createElement('button');
 enter.classList.add('keys');
@@ -242,77 +85,12 @@ enter.classList.add('enter-key');
 enter.innerHTML = "Enter";
 row3.appendChild(enter);
 
-
-// fourth row
-
-const row4 = document.querySelectorAll('.row')[3];
-
 const shift = document.createElement('button');
 shift.classList.add('keys');
 shift.classList.add('shift-key');
 shift.classList.add('shift-left');
 shift.innerHTML = 'Shift';
-row4.appendChild(shift);
-
-const z = document.createElement('button');
-z.classList.add('keys');
-z.innerHTML = 'z';
-row4.appendChild(z);
-
-const x = document.createElement('button');
-x.classList.add('keys');
-x.innerHTML = 'x';
-row4.appendChild(x);
-
-const xButton = document.createElement('button');
-xButton.classList.add('keys');
-xButton.innerHTML = 'x';
-row4.appendChild(xButton);
-
-const c = document.createElement('button');
-c.classList.add('keys');
-c.innerHTML = 'c';
-row4.appendChild(c);
-
-const v = document.createElement('button');
-v.classList.add('keys');
-v.innerHTML = 'v';
-row4.appendChild(v);
-
-const b = document.createElement('button');
-b.classList.add('keys');
-b.innerHTML = 'b';
-row4.appendChild(b);
-
-const n = document.createElement('button');
-n.classList.add('keys');
-n.innerHTML = 'n';
-row4.appendChild(n);
-
-const m = document.createElement('button');
-m.classList.add('keys');
-m.innerHTML = 'm';
-row4.appendChild(m);
-
-const comma = document.createElement('button');
-comma.classList.add('keys');
-comma.innerHTML = ',';
-row4.appendChild(comma);
-
-const left = document.createElement('button');
-left.classList.add('keys');
-left.innerHTML = '<';
-row4.appendChild(left);
-
-const right = document.createElement('button');
-right.classList.add('keys');
-right.innerHTML = '>';
-row4.appendChild(right);
-
-const question = document.createElement('button');
-question.classList.add('keys');
-question.innerHTML = '?';
-row4.appendChild(question);
+row4.prepend(shift);
 
 const shiftRight = document.createElement('button');
 shiftRight.classList.add('keys');
@@ -323,7 +101,6 @@ row4.appendChild(shiftRight);
 
 
 // fifth row
-
 
 const row5 = document.querySelectorAll('.row')[4];
 
